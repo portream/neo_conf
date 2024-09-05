21f0b75d335bfa6398dd674c1f7d77feaefc1a51
Sergei <serve@Sergeis-MacBook-Pro.local>
2024-09-06 01:35:08 +0700

    new config

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -21,17 +21,19 @@ return {
     opts = function(_, opts)
       -- customize the dashboard header
       opts.section.header.val = {
-        " █████  ███████ ████████ ██████   ██████",
-        "██   ██ ██         ██    ██   ██ ██    ██",
-        "███████ ███████    ██    ██████  ██    ██",
-        "██   ██      ██    ██    ██   ██ ██    ██",
-        "██   ██ ███████    ██    ██   ██  ██████",
-        " ",
-        "    ███    ██ ██    ██ ██ ███    ███",
-        "    ████   ██ ██    ██ ██ ████  ████",
-        "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-        "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-        "    ██   ████   ████   ██ ██      ██",
+        "███████╗██╗   ██╗ ██████╗██╗  ██╗",
+        "██╔════╝██║   ██║██╔════╝██║ ██╔╝",
+        "█████╗  ██║   ██║██║     █████╔╝ ",
+        "██╔══╝  ██║   ██║██║     ██╔═██╗ ",
+        "██║     ╚██████╔╝╚██████╗██║  ██╗",
+        "╚═╝      ╚═════╝  ╚═════╝╚═╝  ╚═╝",
+        "██╗   ██╗ ██████╗ ██╗   ██╗      ",
+        "╚██╗ ██╔╝██╔═══██╗██║   ██║      ",
+        " ╚████╔╝ ██║   ██║██║   ██║      ",
+        "  ╚██╔╝  ██║   ██║██║   ██║      ",
+        "   ██║   ╚██████╔╝╚██████╔╝      ",
+        "   ╚═╝    ╚═════╝  ╚═════╝       ",
+        "                                 ",
       }
       return opts
     end,
@@ -79,5 +81,24 @@ return {
         Rule("a", "a", "-vim")
       )
     end,
+  },
+  { -- This plugin
+    "Zeioth/compiler.nvim",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
+    opts = {},
+  },
+  { -- The task runner we use
+    "stevearc/overseer.nvim",
+    commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    opts = {
+      task_list = {
+        direction = "bottom",
+        min_height = 25,
+        max_height = 25,
+        default_detail = 1,
+      },
+    },
   },
 }
